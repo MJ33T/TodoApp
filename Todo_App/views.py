5cc29056856b980becd80e866781658a315f0873
J33T <muhtasim_jeet@hotmail.com>
2021-03-06 23:16:09 +0600

    Todo Functionality done

--- Todo_App/views.py
+++ Todo_App/views.py
@@ -1,6 +1,24 @@
 from django.shortcuts import render
-from django.template import RequestContext
+from django.utils import timezone
+from Todo_App.models import Todo
+from django.http import HttpResponseRedirect
 
 
 def home(request):
-    return render(request, 'TodoApp/index.html')
+    todo_item = Todo.objects.all().order_by("-added_date")
+    return render(request, 'TodoApp/index.html', {
+        'todo_item': todo_item
+    })
+
+
+def add_todo(request):
+    # print(request.POST)
+    current_date = timezone.now()
+    content = request.POST['text']
+    Todo.objects.create(added_date=current_date, text=content)
+    return HttpResponseRedirect('/')
+
+
+def delete_todo(request, todo_id):
+    Todo.objects.get(id=todo_id).delete()
+    return HttpResponseRedirect('/')
